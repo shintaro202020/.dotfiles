706c1b692b9f989ab2c5d9ee727234311b56bb6e
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-21 21:05:31 +0900

    [UPD] sth

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ echo 'export ZDOTDIR=$HOME/.dotfiles/.zsh' >> ~/.zshenv
 export ZDOTDIR=$HOME/.dotfiles/.zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-# setopt EXTENDED_GLOB
+## setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+echo "source ${ZDOTDIR:-$HOME}/.zprezto/init.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 npm install --global pure-prompt
 
-echo "source \"${ZDOTDIR:-$HOME}/.zshcommonrc\"" >>! ${ZDOTDIR:-$HOME}/.zshrc
+echo "source ${ZDOTDIR:-$HOME}/.zshcommonrc" >>! ${ZDOTDIR:-$HOME}/.zshrc
 echo "source ${ZDOTDIR:-$HOME}/.zshcommonlogout" >>! ${ZDOTDIR:-$HOME}/.zlogout
-echo 'source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"' >> ${ZDOTDIR:-$HOME}/.zshrc
 
 ### Set Vim
 ln -fs ~/.dotfiles/.vimrc ~/.vimrc
