a4a14a6efb0aa2ab3e01ef3315ae24b3a6ee95bd
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-21 19:04:49 +0900

    [UPD] iceberg place

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 ### Git clone dotfiles Repository
 ### after install git, curl, vim
 chsh -s $(which zsh)
+mkdir .vim && mkdir .vim/colors
 ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fs ~/.dotfiles/.vimrc ~/.vimrc
-ln -fc ~/.dotfiles/vim/colors ~/.vim/colors
+ln -fc ~/.dotfiles/.vim/colors ~/.vim/colors
 source ~/.zshrc
 pyenv install 3.7.4
 pyenv global 3.7.4
 
 pip install --user pipenv
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 
