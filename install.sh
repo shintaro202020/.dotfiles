a92680a216cbd7478498465c5d34e740bb49b480
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-21 19:46:47 +0900

    [UPD] install

--- install.sh
+++ install.sh
@@ -3,18 +3,20 @@
 mkdir .vim && mkdir .vim/colors
 
 ### Set zsh Root Directory
-echo 'export ZDOTDIR=$HOME/.dotfiles/.zsh' >> ~/.zshenv  
+echo 'export ZDOTDIR=$HOME/.dotfiles/.zsh' >> ~/.zshenv
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-ln -fs ~/.dotfiles/.vimrc ~/.vimrc
-ln -fs ~/.dotfiles/.vim/colors ~/.vim/colors
-ln -fs ~/.dotfiles/.zsh/.zshcommonlogout ~/.zsh/zshcommonlogout
-ln -fs ~/.dotfiles/.zsh/.zshcommonrc ~/.zsh/zshcommonrc
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 echo "source \"${ZDOTDIR:-$HOME}/.zshcommonrc\"" >>! ${ZDOTDIR:-$HOME}/.zshrc
 echo "source ${ZDOTDIR:-$HOME}/.zshcommonlogout" >>! ${ZDOTDIR:-$HOME}/.zlogout
+source ~/.dotfiles/.zsh/.zshrc
+
+### Set Vim
+ln -fs ~/.dotfiles/.vimrc ~/.vimrc
+ln -fs ~/.dotfiles/.vim/colors ~/.vim/colors 
+
 pyenv install 3.7.4
 pyenv global 3.7.4
 
