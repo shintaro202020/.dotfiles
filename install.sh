a92e83343025f1d371d8706273422e7ebe31394a
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-21 18:16:56 +0900

    [UPD] install option

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 ### Git clone dotfiles Repository
+### after install git, curl, vim
 DOTPATH=~/.dotfiles 
 git clone git@github.com:Shintaro0920/.dotfiles.git DOTPATH
+chsh -s $(which zsh)
+ln -fs ~/.dotfiles/.zshrc ~/.zshrc
+ln -fs ~/.dotfiles/.vimrc ~/.vimrc
+pyenv install 3.7.4
+pyenv global 3.7.4
+
+pip install --user pipenv
 
 
