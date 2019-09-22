ed630caac087b2441b261165f0b52b1910815ce0
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-22 23:31:06 +0900

    2019/09/22(Sun) 23:31

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -4,7 +4,7 @@ git config --global user.email "yoshidashintaro0920@gmail.com"
 git config --global user.name  "Shintaro Yoshida" 
 
 ### git clone 
-git clone git@github.com:Shintaro0920/.dotfiles.git ~/.dotfiles
+# git clone git@github.com:Shintaro0920/.dotfiles.git ~/.dotfiles
 
 ### zshenv
 echo "### Setup zsh Root Directory \nexport ZDOTDIR=$HOME/.dotfiles/.zsh" >>! ~/.zshenv
