b40277b00015aae90f525bf8049ce2ddcce0db35
Shintaro Yoshida <yoshidashintaro0920@gmail.com>
2019-09-23 00:09:17 +0900

    [UPD]

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -37,40 +37,28 @@ rm -f $ZDOTDIR/.zpreztorc.bak
 sed -i.bak "s/'prompt'/'syntax-highlighting' 'history-substring-search' 'autosuggestions' 'prompt'/g" $ZDOTDIR/.zpreztorc
 rm -f $ZDOTDIR/.zpreztorc.bak 
 echo '\n###Setup Pure \nautoload -U promptinit; promptinit \nprompt pure' >>! ${ZDOTDIR:-$HOME}/.zshrc
-```                                                                                                                                    
+
 ### Setup zshrc and zlogout
-```
 echo "source ${ZDOTDIR:-$HOME}/.zshcommonrc" >>! ${ZDOTDIR:-$HOME}/.zshrc
 echo "source ${ZDOTDIR:-$HOME}/.zshcommonlogout" >>! ${ZDOTDIR:-$HOME}/.zlogout
-```
 
 ### Setup Vim
-```
 ln -fs ~/.dotfiles/.vim/.vimrc ~/.vimrc
 ln -fs ~/.dotfiles/.vim/colors ~/.vim/
-```
 
 ### Install pyenv and pipenv
-```
 git clone https://github.com/pyenv/pyenv.git $DOTDIR/.pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git $DOTDIR/.pyenv/plugins/pyenv-virtualenv
-```
 
 ### Reload Setup
-```
 source ~/.dotfiles/.zsh/.zshrc
-```
 
 ### Install python
-```
 # pyenv install 3.7.4
 # pyenv install 3.6.8
 # pyenv global 3.7.4
 # pip install --upgrade pip 
 # pip install --user --upgrade pipenv
-```
 
 ### Install atom
-```
 # apm install --packages-file $DOTDIR/.atom/atomfile.txt
-```
